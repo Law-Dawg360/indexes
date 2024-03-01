cbf2f7ca74777200f5dafc56ae88d5f3e29cd2d8
Law-Dawg360 <107430641+Law-Dawg360@users.noreply.github.com>
2024-03-01 09:23:08 -0500

    Create readme.py

---
+++ assets/icons/svg/1/YouTube1/readme.py
@@ -0,0 +1,19 @@
+<img src="/assets/icons/svg/1/YouTube1/big_logo_project_1_iconForCode.svg" align="left" width="200px"/>
+<details open>
+  <summary>big_logo_project_1</summary>
+<img align="left" width="100" height="100" src="/assets/icons/svg/1/YouTube1/big_logo_project_1_iconForCode.svg">
+</details>
+<details open>
+  <summary>big_logo_project_1</summary>
+<img align="left" width="100" height="100" src="/assets/icons/svg/1/YouTube1/big_logo_project_1_iconForCode.svg">
+</details>
+
+<br clear="left"/>
+
+<img src="/assets/icons/svg/1/YouTube1/big_logo_project_1_iconForCode.svg" align="left" width="200px"/>
+<object data="/assets/icons/svg/1/YouTube1/big_logo_project_1_iconForCode.svg" align="left" width="200px"></object>
+<details open>
+  <summary>big_logo_project_1</summary>
+<img align="left" width="100" height="100" src="/assets/icons/svg/1/YouTube1/big_logo_project_1_iconForCode.svg">
+</details>
+<br clear="left"/>
